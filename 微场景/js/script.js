f589ddb26b473b469cbc38f8afba56b0e2d6914a
Songshaopeng7 <3324533267@qq.com>
2017-12-08 08:45:07 +0800

    保存动画

--- 微场景/js/script.js
+++ 微场景/js/script.js
@@ -14,4 +14,16 @@ var swiper = new Swiper('.swiper-container', {
         el: '.swiper-pagination',
         clickable: true,
     },
+
+//   swiper4 animate里的 js插件代码
+    on:{
+        init: function(){
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    }
 });
+
