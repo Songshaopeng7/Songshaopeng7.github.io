a8475cd8fb4543fe813651b474c9882a3919ac5c
Songshaopeng7 <3324533267@qq.com>
2017-12-15 09:41:22 +0800

    完成

--- 双选会/js/script.js
+++ 双选会/js/script.js
@@ -4,4 +4,36 @@ var swiper = new Swiper('.swiper-container', {
         el: '.swiper-pagination',
         clickable: true,
     },
+
+    //   swiper4 animate里的 js插件代码
+    on:{
+        init: function(){
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    }
 });
+
+//音频标签；
+var mp3 = document.querySelector("#mp3");
+
+//音乐按钮
+var mp3Btn = document.querySelector("#mp3Btn");
+
+mp3Btn.onclick = function(){
+    if (mp3.paused) {
+        //开始旋转
+        mp3Btn.style.animationPlayState = 'running';
+        //开始音频
+        mp3.play();
+    } else {
+        //暂停旋转
+        mp3Btn.style.animationPlayState = 'paused';
+        //暂停音频
+        mp3.pause();
+    }
+}
+
